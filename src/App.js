ec1bd7e36b7d5e86922e0d26a6f25dae775aafa4
tomasyovino <tomasyovino@gmail.com>
2023-03-17 15:58:14 -0300

    Display HomePage & Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CartProvider from "./context/CartProvider";
 import { HomePage, CartPage, ContactPage, NotFoundPage } from "./pages";
+import { Navbar } from "./pages/components";
 
 const App = () => {
   return (
     <Router>
       <CartProvider>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/cart" element={<CartPage />} />
